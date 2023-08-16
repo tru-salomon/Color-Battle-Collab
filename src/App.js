750e810f25614b024f0dffb69a739de70300dae4
Jeremy Montes <jeremymontes@pursuit.org>
2023-08-16 12:59:29 -0400

    Added header and gameboardcard components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Header.jsx';
 
 function App() {
   return (
@@ -7,7 +8,7 @@ function App() {
       <header className="App-header">
         <div>
           <header>
-            <h1>Color Battle</h1>
+           <Header />
           </header>
         </div>
       </header>
---
+++ src/Components/GameBoardCard.jsx
@@ -0,0 +1,6 @@
+function GameBoardCard(){
+
+return(<>
+
+</>)
+}
